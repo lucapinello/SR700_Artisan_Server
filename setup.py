3ddb15e504e5e291e48e489d6b93f42ede94a067
Luca Pinello <lucapinello@gmail.com>
2019-01-20 15:35:49 -0500

    add option no phidget

--- setup.py
+++ setup.py
@@ -12,16 +12,18 @@ import glob
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-scripts=glob.glob('cmds/*')
+scripts=glob.glob('SR700-Artisan-PDServer-Phidget/*.py')
 
 setup(
     name='SR700-Artisan-PDServer-Phidget',
     version=0.1,
+    packages = ["SR700-Artisan-PDServer-Phidget"],
+    package_dir={'SR700-Artisan-PDServer-Phidget': '.'},
+    include_package_data = True,
     description='Extension of the SR700-Artisan-PDServer (https://github.com/infinigrove/SR700-Artisan-PDServer) to use with the Phidget',
     url='https://github.com/lucapinello/freshroastsr700_phidget',
     author='Luca Pinello',
     license='GPLv3',
-    packages=find_packages(),
     install_requires=['freshroastsr700_phidget>=0.2','Pyro4','datetime'],
     scripts=scripts,
 )
