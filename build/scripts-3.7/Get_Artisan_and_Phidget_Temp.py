66706ad9b04a98df3411a3375d93158daaab463f
Luca Pinello <lucapinello@gmail.com>
2019-01-21 00:44:00 -0500

    gitignore

--- build/scripts-3.7/Get_Artisan_and_Phidget_Temp.py
+++
@@ -1,9 +0,0 @@
-#!/Users/luca/anaconda3/bin/python
-# -*- coding: utf-8 -*-
-# SR700-Artisan-PDServer, released under GPLv3
-# Get_Roaster_State
-import Pyro4
-
-roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
-et,bt=roast_control.output_sr700_and_phidget_temp()
-print ("%d,%d" %(et,bt))
--- build/scripts-3.7/Roaster_Stop.py
+++
@@ -1,9 +0,0 @@
-#!/Users/luca/anaconda3/bin/python
-# -*- coding: utf-8 -*-
-# SR700-Artisan-PDServer, released under GPLv3
-# Roaster Run Recipe
-
-import Pyro4
-
-roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
-roast_control.stop()
