164d2716b6fd18ca1319adf9dfccabcf97fc6dd4
Luca Pinello <lucapinello@gmail.com>
2019-01-21 20:03:58 -0500

    fixed scripts

--- SR700_Artisan_Server/Get_Artisan_and_Phidget_Temp.py
+++ SR700_Artisan_Server/Get_Artisan_and_Phidget_Temp.py
@@ -4,6 +4,7 @@
 # Get_Roaster_State
 import Pyro4
 
-roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
-et,bt=roast_control.output_sr700_and_phidget_temp()
-print ("%d,%d" %(et,bt))
+if __name__ == '__main__':
+    roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
+    et,bt=roast_control.output_sr700_and_phidget_temp()
+    print ("%d,%d" %(et,bt))
