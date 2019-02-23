dfdbe6d07b758fa849d701054595219168f32cc6
Luca Pinello <lucapinello@gmail.com>
2019-02-22 22:59:06 -0500

    manual mode

--- SR700_Artisan_Server/Roaster_Cooling.py
+++ SR700_Artisan_Server/Roaster_Cooling.py
@@ -7,6 +7,8 @@ import Pyro4
 
 def main():
     roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
+    roast_control.enable_temp_manual_mode()
+    roast_control.enable_fan_manual_mode()
     roast_control.set_fan_speed(9)
     roast_control.run_cooling()
 
