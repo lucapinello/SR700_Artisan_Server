4891d8b46bf6c89b99c3bf9217d929db62c62ea8
lucapinello <lucapinello@gmail.com>
2019-08-31 19:52:21 -0400

    Preheat to 200

--- SR700_Artisan_Server/Roaster_Preheat.py
+++ SR700_Artisan_Server/Roaster_Preheat.py
@@ -9,7 +9,7 @@ def main():
     roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
 
     roast_control.set_time(1500)
-    roast_control.set_temperature(120)
+    roast_control.set_temperature(200)
     roast_control.set_fan_speed(9)
     roast_control.run_roast()
 
