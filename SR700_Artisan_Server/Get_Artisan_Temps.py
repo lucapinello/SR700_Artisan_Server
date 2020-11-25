4366eae782c2861c9633bd555ffe6043e25abf02
Álvaro Gomides <alvaro.gomides@zarpo.com.br>
2020-11-25 09:48:25 -0300

    Transform fareinheint to celsius

--- SR700_Artisan_Server/Get_Artisan_Temps.py
+++ SR700_Artisan_Server/Get_Artisan_Temps.py
@@ -7,6 +7,9 @@ import Pyro4
 def main():
     roast_control = Pyro4.Proxy("PYRONAME:roaster.sr700")
     et,bt=roast_control.output_temps()
+    #Celsius = (Fahrenheit – 32) * 5/9
+    et = (et - 32) * 5/9
+    bt = (bt - 32) * 5/9
     print ("%d,%d" %(et,bt)) #in manual mode the values are negative.
 
 if __name__ == '__main__':
